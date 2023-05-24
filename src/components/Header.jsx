a3538a7e5089e3530d597e0f6e7e4c34728c8139
jsapro <77.3.77@mail.ru>
2023-05-24 14:48:57 +0300

    add Navlink to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "../images/header-logo.svg";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="лого Mesto" />
+      <NavLink to="/main" className="header__link">Регистрация</NavLink>
     </header>
   );
 }
